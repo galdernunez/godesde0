1beaaf9a4e11c6a9605fc59a95f4482f5c093255
galder.nunez.next <galder.nunez.next@bbva.com>
2023-07-19 14:40:02 +0200

    condiciones

--- main.go
+++ main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
-	"godesde0/variables"
+	"runtime"
 )
 
 func main() {
-	estado, texto := variables.ConviertoATexto(1596)
+	/*estado, texto := variables.ConviertoATexto(1596)
 	fmt.Println(estado)
-	fmt.Println(texto)
+	fmt.Println(texto)*/
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("esto no es Win")
+	} else {
+		fmt.Println("esto es Win")
+	}
+
+	switch os := runtime.GOARCH; os {
+	case "intel":
+		fmt.Println("Esto es Intel")
+	case "amd":
+		fmt.Println("Esto es amd")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
