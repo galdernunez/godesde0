e4fae0d1f72c008e75340772823483a451863d7b
galder.nunez.next <galder.nunez.next@bbva.com>
2023-07-19 14:27:09 +0200

    Next Commit

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "godesde0/variables"
+import (
+	"fmt"
+	"godesde0/variables"
+)
 
 func main() {
-	variables.MuestraEnteras()
+	estado, texto := variables.ConviertoATexto(1596)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
